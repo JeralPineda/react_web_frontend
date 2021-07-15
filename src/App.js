7f00978aba6a64f2f2cf9beb9cbe4be7a9553b78
Jeral <jeral.cris@gmail.com>
2021-07-15 15:33:45 -0600

    Probando componentes de ant design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { DatePicker, Card } from 'antd';
 import './App.scss';
 
+
 function App() {
+    const test = (date) => {
+        console.log(date);
+    }
   return (
     <div className="app">
       <h1>Web Personal - Frontend</h1>
       <h2>Proyecto</h2>
+
+      <DatePicker  onChange={test} />
+
+      <Card title="Default size card" extra={<a href="/#">Test</a>} style={{ width: 300 }}>
+      <p>Hola Mundo</p>
+      
+    </Card>
     </div>
   );
 }
