0611864c623ffd524670452b4c5733b5f8f19592
Jeral <jeral.cris@gmail.com>
2021-07-18 17:00:20 -0600

    Componentes para las rutas

--- src/App.js
+++ src/App.js
@@ -1,38 +1,21 @@
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Admin from './pages/Admin';
+import SignIn from './pages/Admin/SignIn';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
 
 import './App.scss';
 
 function App() {
    return (
-      <Router>
-         <div className='app'>
-            <h1>Sistema de rutas básicas</h1>
-            <Link to='/'>Home</Link> <br />
-            <Link to='/contact'>Contact</Link> <br />
-            <Link to='/users'>Users</Link>
-            <Switch>
-               <Route exact path='/' component={Home} />
-               <Route exact path='/contact' component={Contact} />
-               <Route exact path='/users' component={Users} />
-               <Route component={Error404} />
-            </Switch>
-         </div>
-      </Router>
+      <>
+         <h1>App</h1>
+
+         <Admin />
+         <SignIn />
+         <Home />
+         <Contact />
+      </>
    );
-}
-
-function Home() {
-   return <h2>Home</h2>;
-}
-
-function Contact() {
-   return <h2>Contact</h2>;
-}
-function Users() {
-   return <h2>Users</h2>;
-}
-function Error404() {
-   return <h2>Error 404</h2>;
 }
 
 export default App;
