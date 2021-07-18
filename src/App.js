f9e42ab8b55b141b6653ede737f6d71786b09a9e
Jeral <jeral.cris@gmail.com>
2021-07-18 16:24:32 -0600

    Ejemplo de rutas con react router dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import { DatePicker, Card } from 'antd';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 import './App.scss';
 
-
 function App() {
-    const test = (date) => {
-        console.log(date);
-    }
-  return (
-    <div className="app">
-      <h1>Web Personal - Frontend</h1>
-      <h2>Proyecto</h2>
+   return (
+      <Router>
+         <div className='app'>
+            <h1>Sistema de rutas básicas</h1>
+            <Link to='/'>Home</Link> <br />
+            <Link to='/contact'>Contact</Link> <br />
+            <Link to='/users'>Users</Link>
+            <Switch>
+               <Route exact path='/' component={Home} />
+               <Route exact path='/contact' component={Contact} />
+               <Route exact path='/users' component={Users} />
+               <Route component={Error404} />
+            </Switch>
+         </div>
+      </Router>
+   );
+}
 
-      <DatePicker  onChange={test} />
+function Home() {
+   return <h2>Home</h2>;
+}
 
-      <Card title="Default size card" extra={<a href="/#">Test</a>} style={{ width: 300 }}>
-      <p>Hola Mundo</p>
-      
-    </Card>
-    </div>
-  );
+function Contact() {
+   return <h2>Contact</h2>;
+}
+function Users() {
+   return <h2>Users</h2>;
+}
+function Error404() {
+   return <h2>Error 404</h2>;
 }
 
 export default App;
