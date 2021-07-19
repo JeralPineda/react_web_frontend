998f2b02b7b28b97494e66849b062167a240fc3e
Jeral <jeral.cris@gmail.com>
2021-07-18 18:32:21 -0600

    Sistema de rutas para cargar las paginas dentro del LayoutAdmin

--- src/layouts/LayoutAdmin.js
+++ src/layouts/LayoutAdmin.js
@@ -1,14 +1,36 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import { Layout } from 'antd';
 
-const LayoutAdmin = () => {
+import './LayoutAdmin.scss';
+
+const LayoutAdmin = ({ routes }) => {
+   //    console.log(routes);
+   const { Header, Content, Footer } = Layout;
    return (
       <Layout>
-         <h2>Menu Sider</h2>
-         <div>Contenido....</div>
-         <h5>Footer</h5>
+         <h2>Menu Sider Admin</h2>
+
+         <Layout>
+            <Header>Header</Header>
+            <Content>
+               <LoadRouters routes={routes} />
+            </Content>
+            <Footer>Footer</Footer>
+         </Layout>
       </Layout>
    );
 };
+
+function LoadRouters({ routes }) {
+   return routes.map((route, index) => (
+      <Route
+         key={index} //
+         path={route.path}
+         exact={route.exact}
+         component={route.component}
+      />
+   ));
+}
 
 export default LayoutAdmin;
