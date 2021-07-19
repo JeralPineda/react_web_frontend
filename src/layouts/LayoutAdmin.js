c58b29b004e9df4e668f32d8c4622ea43b351839
Jeral <jeral.cris@gmail.com>
2021-07-18 19:17:18 -0600

    limpiando log

--- src/layouts/LayoutAdmin.js
+++ src/layouts/LayoutAdmin.js
@@ -5,17 +5,19 @@ import { Layout } from 'antd';
 import './LayoutAdmin.scss';
 
 const LayoutAdmin = ({ routes }) => {
-   //    console.log(routes);
    const { Header, Content, Footer } = Layout;
+
    return (
       <Layout>
          <h2>Menu Sider Admin</h2>
 
          <Layout>
             <Header>Header</Header>
+
             <Content>
                <LoadRouters routes={routes} />
             </Content>
+
             <Footer>Footer</Footer>
          </Layout>
       </Layout>
