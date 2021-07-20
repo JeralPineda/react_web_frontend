625858ba7391e9717f333d9e7e62d0c79106d6ce
Jeral <jeral.cris@gmail.com>
2021-07-19 22:08:13 -0600

    Creando el componente MenuTop

---
+++ src/components/menuTop/MenuTop.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Button } from 'antd';
+import { MenuUnfoldOutlined, PoweroffOutlined } from '@ant-design/icons';
+
+import JeralPineda from '../../assets/img/png/logo-white.png';
+
+import './MenuTop.scss';
+
+const MenuTop = () => {
+   return (
+      <div className='menu-top'>
+         <div className='menu-top__left'>
+            <img className='menu-top__left-logo' src={JeralPineda} alt='logo' />
+
+            <Button type='link' onClick={() => console.log('click')}>
+               <MenuUnfoldOutlined />
+            </Button>
+         </div>
+         <div className='menu-top__right'>
+            <Button type='link' onClick={() => console.log('cerrar sesión')}>
+               <PoweroffOutlined />
+            </Button>
+         </div>
+      </div>
+   );
+};
+
+export default MenuTop;
