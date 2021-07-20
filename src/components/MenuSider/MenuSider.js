7bd69303fc5ba34662ab0e1d110c9c1b79409883
Jeral <jeral.cris@gmail.com>
2021-07-20 08:48:26 -0600

    haciendo que el menu sider sea collapsable

--- src/components/MenuSider/MenuSider.js
+++ src/components/MenuSider/MenuSider.js
@@ -5,22 +5,22 @@ import { HomeOutlined, MenuOutlined } from '@ant-design/icons';
 import './MenuSider.scss';
 import { Link } from 'react-router-dom';
 
-const MenuSider = () => {
+const MenuSider = ({ menuCollapsed }) => {
    const { Sider } = Layout;
 
    return (
-      <Sider className='menu-sider'>
+      <Sider className='menu-sider' collapsed={menuCollapsed}>
          <Menu theme='dark' mode='inline' defaultSelectedKeys={['1']}>
             <Menu.Item key='1'>
                <Link to={'/admin'}>
                   <HomeOutlined />
-                  <span className='nav-text'>Home</span>
+                  <span className='nav-text'> Home</span>
                </Link>
             </Menu.Item>
             <Menu.Item key='2'>
                <Link to={'/admin/login'}>
                   <MenuOutlined />
-                  <span className='nav-text'>Menu web</span>
+                  <span className='nav-text'> Menu web</span>
                </Link>
             </Menu.Item>
          </Menu>
