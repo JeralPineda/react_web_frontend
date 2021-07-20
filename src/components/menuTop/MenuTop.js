650424b65f1e66d838173dfa9268d6c793ba1422
Jeral <jeral.cris@gmail.com>
2021-07-20 08:49:30 -0600

    funcion para cambiar el estado al dar click en el boton menu y collapsar el sider

--- src/components/menuTop/MenuTop.js
+++ src/components/menuTop/MenuTop.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Button } from 'antd';
-import { MenuOutlined, PoweroffOutlined } from '@ant-design/icons';
+import { MenuFoldOutlined, MenuUnfoldOutlined, PoweroffOutlined } from '@ant-design/icons';
 
 import JeralPineda from '../../assets/img/png/logo-white.png';
 
 import './MenuTop.scss';
 
-const MenuTop = () => {
+const MenuTop = ({ menuCollapsed, setMenuCollapsed }) => {
+   const handleMenuCollapsed = () => {
+      setMenuCollapsed(!menuCollapsed);
+   };
+
    return (
       <div className='menu-top'>
          <div className='menu-top__left'>
             <img className='menu-top__left-logo' src={JeralPineda} alt='logo' />
 
-            <Button type='link' onClick={() => console.log('click')}>
-               <MenuOutlined />
+            <Button type='link' onClick={handleMenuCollapsed}>
+               {menuCollapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
             </Button>
          </div>
          <div className='menu-top__right'>
