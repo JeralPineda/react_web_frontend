6a4b81705f42976977aff1ce611d23a7e1a84bf7
Jeral <jeral.cris@gmail.com>
2021-07-20 20:37:50 -0600

    SigIn estructura basica con Tabs

---
+++ src/pages/Admin/SignIn/SignIn.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { Layout, Tabs } from 'antd';
+
+import logo from '../../../assets/img/png/logo-white.png';
+
+import './SignIn.scss';
+
+const SignIn = () => {
+   const { Content } = Layout;
+   const { TabPane } = Tabs;
+
+   return (
+      <Layout className='sign-in'>
+         <Content className='sign-in__content'>
+            <h1 className='sign-in__content-logo'>
+               <img src={logo} alt='logo' />
+            </h1>
+
+            <div className='sign-in__content-tabs'>
+               <Tabs type='card'>
+                  <TabPane tab={<span>Entrar</span>} key='1'>
+                     componente loginform
+                  </TabPane>
+                  <TabPane tab={<span>Nuevo usuario</span>} key='2'>
+                     componente RegisterForm
+                  </TabPane>
+               </Tabs>
+            </div>
+         </Content>
+      </Layout>
+   );
+};
+
+export default SignIn;
