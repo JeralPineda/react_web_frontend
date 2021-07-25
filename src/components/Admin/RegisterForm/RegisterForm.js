bc0615bb0f4f9f71993d04eb89b72198193df564
Jeral <jeral.cris@gmail.com>
2021-07-25 15:22:53 -0600

    Reutilizando las funciones de validacion en el formulario de registro

--- src/components/Admin/RegisterForm/RegisterForm.js
+++ src/components/Admin/RegisterForm/RegisterForm.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-import { Form, Input, Button, Checkbox, notification } from 'antd';
+import { Form, Input, Button, Checkbox } from 'antd';
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
+
+import { emailValidation, minLengthValidation } from 'utils/formValidation';
 
 import './RegisterForm.scss';
 
@@ -13,6 +15,13 @@ const RegisterForm = () => {
       email: '',
       password: '',
       repeatPassword: '',
+      privacyPolicy: false,
+   });
+
+   const [formValid, setFormValid] = useState({
+      email: false,
+      password: false,
+      repeatPassword: false,
       privacyPolicy: false,
    });
 
@@ -31,9 +40,34 @@ const RegisterForm = () => {
       }
    };
 
+   const inputVlidation = ({ target }) => {
+      const { type, name } = target;
+
+      if (type === 'email') {
+         setFormValid({
+            ...formValid,
+            [name]: emailValidation(target),
+         });
+      }
+
+      if (type === 'password') {
+         setFormValid({
+            ...formValid,
+            [name]: minLengthValidation(target, 6),
+         });
+      }
+
+      if (type === 'checkbox') {
+         setFormValid({
+            ...formValid,
+            [name]: target.checked,
+         });
+      }
+   };
+
    const register = (e) => {
       e.preventDefault();
-      console.log(inputs);
+      console.log(formValid);
    };
 
    return (
@@ -45,6 +79,7 @@ const RegisterForm = () => {
                name='email'
                placeholder='Correo'
                className='register-form__input'
+               onChange={inputVlidation}
                value={inputs.email}
             />
          </Form.Item>
@@ -56,6 +91,7 @@ const RegisterForm = () => {
                name='password'
                placeholder='Contraseña'
                className='register-form__input'
+               onChange={inputVlidation}
                value={inputs.password}
             />
          </Form.Item>
@@ -67,6 +103,7 @@ const RegisterForm = () => {
                name='repeatPassword'
                placeholder='Repetir contraseña'
                className='register-form__input'
+               onChange={inputVlidation}
                value={inputs.repeatPassword}
             />
          </Form.Item>
@@ -74,6 +111,7 @@ const RegisterForm = () => {
          <Form.Item>
             <Checkbox
                name='privacyPolicy' //
+               onChange={inputVlidation}
                checked={inputs.privacyPolicy}
             >
                He leído y acepto las politicas de privacidad.
