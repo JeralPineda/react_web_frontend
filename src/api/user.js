d067aa243ddcd082b3c7b83df2391d8168755662
Jeral <jeral.cris@gmail.com>
2021-08-16 20:33:43 -0600

    Funcion para logear usuario que conecte con el api

--- src/api/user.js
+++ src/api/user.js
@@ -32,3 +32,27 @@ export const signUpApi = (data) => {
          };
       });
 };
+
+export const signIn = (data) => {
+   const url = `${basePath}/${apiVersion}/auth/signin`;
+
+   const params = {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+         'Content-Type': 'application/json',
+      },
+   };
+
+   return fetch(url, params)
+      .then((response) => {
+         return response.json();
+      })
+      .then((result) => {
+         console.log(result);
+         return result;
+      })
+      .catch((err) => {
+         return err.message;
+      });
+};
