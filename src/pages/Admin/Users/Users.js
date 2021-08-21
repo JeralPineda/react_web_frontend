12d9aedd12225317ad759c8255946c672be29fde
Jeral <jeral.cris@gmail.com>
2021-08-20 22:43:26 -0600

    useState y useEffect para ejecuar la funcionque obtiene los usuarios

--- src/pages/Admin/Users/Users.js
+++ src/pages/Admin/Users/Users.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { getAccessTokenApi } from 'api/auth';
+import { getUsersApi } from 'api/user';
 
 import './Users.scss';
 
 const Users = () => {
+   const [users, setUsers] = useState([]);
+   const token = getAccessTokenApi();
+
+   console.log(users);
+
+   useEffect(() => {
+      getUsersApi(token).then((response) => {
+         setUsers(response);
+      });
+   }, [token]);
+
    return (
       <div>
          <h1>Lista de usuarios</h1>
