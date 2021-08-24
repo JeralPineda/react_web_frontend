77d41ea83b7c5e87f2a76ff30f65b55dc178e443
Jeral <jeral.cris@gmail.com>
2021-08-23 21:52:19 -0600

    Creando componente para mostrar el avatar en la lista de usuarios inactivos

---
+++ src/components/Admin/Users/ListUsers/UserInactive.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+import { List, Avatar, Button } from 'antd';
+import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
+
+import NoAvatar from 'assets/img/png/no-avatar.png';
+import { getAvatarApi } from 'api/user';
+
+export const UserInactive = ({ user }) => {
+   const [avatar, setAvatar] = useState(null);
+
+   useEffect(() => {
+      if (user.avatar) {
+         getAvatarApi(user.avatar).then((response) => {
+            setAvatar(response);
+         });
+      } else {
+         setAvatar(null);
+      }
+   }, [user]);
+
+   const editUser = () => {
+      console.log('Editando usuario');
+   };
+
+   const deleteUser = () => {
+      console.log('Eliminar usuario');
+   };
+   return (
+      <List.Item
+         //
+         actions={[
+            <Button
+               //
+               type='primary'
+               onClick={editUser}
+            >
+               <EditOutlined />
+            </Button>,
+            <Button
+               //
+               type='danger'
+               onClick={deleteUser}
+            >
+               <DeleteOutlined />
+            </Button>,
+         ]}
+      >
+         <List.Item.Meta
+            avatar={<Avatar src={avatar ? avatar : NoAvatar} />}
+            title={`
+                        ${user.name ? user.name : '...'}
+                        ${user.lastName ? user.lastName : '...'}
+                    `}
+            description={user.email}
+         />
+      </List.Item>
+   );
+};
--- src/components/Admin/Users/ListUsers/UsersInactive.js
+++ src/components/Admin/Users/ListUsers/UsersInactive.js
@@ -1,53 +1,15 @@
 import React from 'react';
-import { List, Avatar, Button } from 'antd';
-import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
-
-import NoAvatar from 'assets/img/png/no-avatar.png';
+import { List } from 'antd';
+import { UserInactive } from './UserInactive';
 
 export const UsersInactive = ({ usersInactive }) => {
-   const editUser = () => {
-      console.log('Editando usuario');
-   };
-
-   const deleteUser = () => {
-      console.log('Eliminar usuario');
-   };
-
    return (
       <List
+         //
          className='users-active'
          itemLayout='horizontal'
          dataSource={usersInactive}
-         renderItem={(user) => (
-            <List.Item
-               //
-               actions={[
-                  <Button
-                     //
-                     type='primary'
-                     onClick={editUser}
-                  >
-                     <EditOutlined />
-                  </Button>,
-                  <Button
-                     //
-                     type='danger'
-                     onClick={deleteUser}
-                  >
-                     <DeleteOutlined />
-                  </Button>,
-               ]}
-            >
-               <List.Item.Meta
-                  avatar={<Avatar src={user.avatar ? user.avatar : NoAvatar} />}
-                  title={`
-                        ${user.name ? user.name : '...'}
-                        ${user.lastName ? user.lastName : '...'}
-                    `}
-                  description={user.email}
-               />
-            </List.Item>
-         )}
+         renderItem={(user) => <UserInactive user={user} />}
       />
    );
 };
