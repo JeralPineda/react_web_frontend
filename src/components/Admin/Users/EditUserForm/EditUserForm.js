66593d019e8d8c6166e8764849b52fa477ea1060
Jeral <jeral.cris@gmail.com>
2021-08-23 20:33:21 -0600

    Efectos para traer el avatar desde el servidor y para la data del user

--- src/components/Admin/Users/EditUserForm/EditUserForm.js
+++ src/components/Admin/Users/EditUserForm/EditUserForm.js
@@ -2,23 +2,39 @@ import React, { useEffect, useState } from 'react';
 
 import { UploadAvatar } from './UploadAvatar';
 import { EditForm } from './EditForm';
+import { getAvatarApi } from 'api/user';
 
 import './EditUserForm.scss';
 
 const EditUserForm = ({ user }) => {
    const [avatar, setAvatar] = useState(null);
 
-   const [userData, setUserData] = useState({
-      name: user.name,
-      lastName: user.lastName,
-      email: user.email,
-      role: user.role,
-      avatar: user.avatar,
-   });
+   const [userData, setUserData] = useState({});
+
+   useEffect(() => {
+      setUserData({
+         //
+         name: user.name,
+         lastName: user.lastName,
+         email: user.email,
+         role: user.role,
+         avatar: user.avatar,
+      });
+   }, [user]);
+
+   useEffect(() => {
+      if (user.avatar) {
+         getAvatarApi(user.avatar).then((response) => {
+            setAvatar(response);
+         });
+      } else {
+         setAvatar(null);
+      }
+   }, [user]);
 
    useEffect(() => {
       if (avatar) {
-         setUserData({ ...userData, avatar });
+         setUserData({ ...userData, avatar: avatar.file });
       }
       // eslint-disable-next-line react-hooks/exhaustive-deps
    }, [avatar]);
