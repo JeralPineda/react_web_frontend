3be4c6e327e92185827c444b5579a62cc912b3da
Jeral <jeral.cris@gmail.com>
2021-08-24 12:28:40 -0600

    Enviando el setReloadUser por prop en el componente UserActive

--- src/components/Admin/Users/ListUsers/UsersActive.js
+++ src/components/Admin/Users/ListUsers/UsersActive.js
@@ -10,7 +10,13 @@ export const UsersActive = ({ usersActive, setIsVisibleModal, setModalTitle, set
 
       setModalTitle(`Editar ${user.name ? user.name : '...'} ${user.lastName} ${user.lastName ? user.lastName : '...'} `);
 
-      setModalContent(<EditUserForm user={user} setIsVisibleModal={setIsVisibleModal} setReloadUsers={setReloadUsers} />);
+      setModalContent(
+         <EditUserForm //
+            user={user}
+            setIsVisibleModal={setIsVisibleModal}
+            setReloadUsers={setReloadUsers}
+         />
+      );
    };
 
    return (
@@ -18,7 +24,13 @@ export const UsersActive = ({ usersActive, setIsVisibleModal, setModalTitle, set
          className='users-active'
          itemLayout='horizontal'
          dataSource={usersActive}
-         renderItem={(user) => <UserActive user={user} editUser={editUser} />}
+         renderItem={(user) => (
+            <UserActive //
+               user={user}
+               editUser={editUser}
+               setReloadUsers={setReloadUsers}
+            />
+         )}
       />
    );
 };
