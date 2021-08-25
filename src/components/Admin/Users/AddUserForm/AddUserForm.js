3110cca75df08690bbf886219e1b6f7c93a3548d
Jeral <jeral.cris@gmail.com>
2021-08-24 21:14:05 -0600

    Agregando la funcionalidad para crear un nuevo usuario

--- src/components/Admin/Users/AddUserForm/AddUserForm.js
+++ src/components/Admin/Users/AddUserForm/AddUserForm.js
@@ -10,8 +10,51 @@ import { AddForm } from './AddForm';
 const AddUserForm = ({ setIsVisibleModal, setReloadUsers }) => {
    const [userData, setUserData] = useState({});
 
+   let userUpdate = userData;
+
    const addUser = () => {
-      console.log(userData);
+      if (!userData.name || !userData.lastName || !userData.email || !userData.role || !userData.password || !userData.repeatPassword) {
+         notification['error']({
+            message: 'Todos los campos son obligatorios',
+         });
+      } else if (userData.password !== userData.repeatPassword) {
+         notification['error']({
+            message: 'Las contraseñas tienen que ser iguales ',
+         });
+      } else {
+         const accessToken = getAccessTokenApi();
+
+         //  eliminamos el repeatPassword de la data
+         delete userUpdate.repeatPassword;
+
+         signUpAdminApi(accessToken, userData)
+            .then((response) => {
+               notification['success']({
+                  message: response,
+               });
+
+               //    Cerramos el modal
+               setIsVisibleModal(false);
+
+               //    Actualizamos la lista de usuarios
+               setReloadUsers(true);
+
+               //    Actualizamos la data para limpiar los campos
+               setUserData({
+                  name: '',
+                  lastName: '',
+                  email: '',
+                  role: '',
+                  password: '',
+                  repeatPassword: '',
+               });
+            })
+            .catch((err) => {
+               notification['error']({
+                  message: err,
+               });
+            });
+      }
    };
 
    return (
