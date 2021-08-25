f63f364630e1055f632b3996c9e9de19be01f6b2
Jeral <jeral.cris@gmail.com>
2021-08-24 20:03:39 -0600

    Agregando componente que contiene el formulario y estado para la data

--- src/components/Admin/Users/AddUserForm/AddUserForm.js
+++ src/components/Admin/Users/AddUserForm/AddUserForm.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import { Form, Input, Select, Button, Row, Col, notification } from 'antd';
-import { LockOutlined, MailOutlined, UserOutlined } from '@ant-design/icons';
+import React, { useState } from 'react';
+import { notification } from 'antd';
 
 import { getAccessTokenApi } from 'api/auth';
 import { signUpAdminApi } from 'api/user';
 
 import './AddUserForm.scss';
+import { AddForm } from './AddForm';
 
 const AddUserForm = ({ setIsVisibleModal, setReloadUsers }) => {
+   const [userData, setUserData] = useState({});
+
+   const addUser = () => {
+      console.log(userData);
+   };
+
    return (
-      <div>
-         <h1>Hola mundo</h1>
+      <div className='add-user-form'>
+         <AddForm
+            //
+            userData={userData}
+            setUserData={setUserData}
+            addUser={addUser}
+         />
       </div>
    );
 };
