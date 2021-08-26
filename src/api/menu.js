b182ef2f298d3f2f071b49fa58896fba510c8095
Jeral <jeral.cris@gmail.com>
2021-08-26 16:50:19 -0600

    Funcion para consumir api y activar o desactivar cualquier menu

--- src/api/menu.js
+++ src/api/menu.js
@@ -38,3 +38,27 @@ export const updateMenuApi = (token, menuId, data) => {
          return err.message;
       });
 };
+
+export const activateMenuApi = (token, menuId, status) => {
+   const url = `${basePath}/${apiVersion}/menu/activate-menu/${menuId}`;
+
+   const params = {
+      method: 'PUT',
+      headers: {
+         'Content-Type': 'application/json',
+         'x-token': `Bearer ${token}`,
+      },
+      body: JSON.stringify({ active: status }),
+   };
+
+   return fetch(url, params)
+      .then((response) => {
+         return response.json();
+      })
+      .then((result) => {
+         return result;
+      })
+      .catch((err) => {
+         return err.message;
+      });
+};
