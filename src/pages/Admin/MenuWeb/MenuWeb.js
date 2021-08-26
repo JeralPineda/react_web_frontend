5bfa94ee39cd6073d9c97e82303a8fb74aef34f9
Jeral <jeral.cris@gmail.com>
2021-08-26 11:07:31 -0600

    Componente de la nueva pagina para los menus y guardarlos en un estado

--- src/pages/Admin/MenuWeb/MenuWeb.js
+++ src/pages/Admin/MenuWeb/MenuWeb.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { getMenuApi } from 'api/menu';
 
 const MenuWeb = () => {
+   const [menu, setMenu] = useState([]);
+   const [reloadMenuWeb, setReloadMenuWeb] = useState(false);
+
+   useEffect(() => {
+      getMenuApi().then((response) => {
+         setMenu(response.menu);
+      });
+
+      setReloadMenuWeb(false);
+   }, [reloadMenuWeb]);
+
    return (
       <div className='menu-web'>
          <h1>Menu web</h1>
