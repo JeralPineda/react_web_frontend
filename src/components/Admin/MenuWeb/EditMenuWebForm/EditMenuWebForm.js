c98c372909d37f35006c077ada09290fd48f15c1
Jeral <jeral.cris@gmail.com>
2021-08-27 19:33:27 -0600

    Agregando la funcionalidad para actualizar el menu que estamos editando

--- src/components/Admin/MenuWeb/EditMenuWebForm/EditMenuWebForm.js
+++ src/components/Admin/MenuWeb/EditMenuWebForm/EditMenuWebForm.js
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { notification } from 'antd';
 
 import { updateMenuApi } from 'api/menu';
 import { getAccessTokenApi } from 'api/auth';
-
-import './EditMenuWebForm.scss';
 import { EditForm } from './EditForm';
 
+import './EditMenuWebForm.scss';
+
 const EditMenuWebForm = ({ setIsVisibleModal, setReloadMenuWeb, menu }) => {
+   const [menuWebData, setMenuWebData] = useState(menu);
+
+   useEffect(() => {
+      setMenuWebData(menu);
+   }, [menu]);
+
+   const editMenu = () => {
+      if (!menuWebData.title || !menuWebData.url) {
+         notification['error']({
+            message: 'Todos los campos son obligatorios',
+         });
+      }
+
+      const accessToken = getAccessTokenApi();
+
+      updateMenuApi(accessToken, menuWebData.uid, menuWebData)
+         .then((response) => {
+            notification['success']({
+               message: response,
+            });
+
+            setIsVisibleModal(false); //ocultar modal
+            setReloadMenuWeb(true); //recargar menu y mostrar nuevos valores
+         })
+         .catch((err) => {
+            notification['error']({
+               message: 'Error del servidor',
+            });
+         });
+   };
+
    return (
       <div className='edit-menu-web-form'>
-         <EditForm />
+         <EditForm
+            //
+            menuWebData={menuWebData}
+            setMenuWebData={setMenuWebData}
+            editMenu={editMenu}
+         />
       </div>
    );
 };
