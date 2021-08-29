72dd78f03a5658f734e0988205ed45304612d0ef
Jeral <jeral.cris@gmail.com>
2021-08-29 13:23:11 -0600

    Obteniendo el menu de la base de datos

--- src/components/Web/MenuTop/MenuTop.js
+++ src/components/Web/MenuTop/MenuTop.js
@@ -8,20 +8,52 @@ import { getMenuApi } from 'api/menu';
 import './MenuTop.scss';
 
 const MenuTop = () => {
+   const [menuData, setMenuData] = useState([]);
+
+   useEffect(() => {
+      getMenuApi().then((response) => {
+         const arrayMenu = [];
+
+         response.menu.forEach((item) => {
+            // if (item.active) {
+            //    arrayMenu.push(item);
+            // }
+
+            item.active && arrayMenu.push(item);
+         });
+
+         setMenuData(arrayMenu);
+      });
+   }, []);
+
    return (
-      <Menu className='menu-topp' mode='horizontal'>
-         <Menu.Item className='menu-topp__logo'>
+      <Menu className='menu-top-web' mode='horizontal'>
+         <Menu.Item className='menu-top-web__logo'>
             <Link to={'/'}>
                <img src={LogoWeb} alt='Jeral Pineda' />
             </Link>
          </Menu.Item>
 
-         <Menu.Item className='menu-topp__item'>
-            <Link to={'/'}>Home</Link>
-         </Menu.Item>
-         <Menu.Item className='menu-topp__item'>
-            <Link to={'/contact'}>Contacto</Link>
-         </Menu.Item>
+         {menuData.map((item) => {
+            // detectamos si la url es interna o externa a la web
+            const external = item.url.indexOf('http') > -1 ? true : false;
+
+            if (external) {
+               return (
+                  <Menu.Item key={item.uid} className='menu-top-web__item'>
+                     <a href={item.url} target='_blank' rel='noopener noreferrer'>
+                        {item.title}
+                     </a>
+                  </Menu.Item>
+               );
+            }
+
+            return (
+               <Menu.Item key={item.uid} className='menu-top-web__item'>
+                  <Link to={item.url}>{item.title}</Link>
+               </Menu.Item>
+            );
+         })}
 
          <div>Social Media..</div>
       </Menu>
