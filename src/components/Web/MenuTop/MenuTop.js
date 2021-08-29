bb857236185861aad3376368085ef3873907a93f
Jeral <jeral.cris@gmail.com>
2021-08-29 10:40:04 -0600

    Añadiendo el logo de la web al menu y cambiando las clases

--- src/components/Web/MenuTop/MenuTop.js
+++ src/components/Web/MenuTop/MenuTop.js
@@ -2,17 +2,24 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Menu } from 'antd';
 
+import LogoWeb from '../../../assets/img/png/logo192.png';
+import { getMenuApi } from 'api/menu';
+
 import './MenuTop.scss';
 
 const MenuTop = () => {
    return (
-      <Menu className='menu-top' mode='horizontal'>
-         <Menu.Item className='menu-top__logo'>Logo..</Menu.Item>
+      <Menu className='menu-topp' mode='horizontal'>
+         <Menu.Item className='menu-topp__logo'>
+            <Link to={'/'}>
+               <img src={LogoWeb} alt='Jeral Pineda' />
+            </Link>
+         </Menu.Item>
 
-         <Menu.Item className='menu-top__item'>
+         <Menu.Item className='menu-topp__item'>
             <Link to={'/'}>Home</Link>
          </Menu.Item>
-         <Menu.Item className='menu-top__item'>
+         <Menu.Item className='menu-topp__item'>
             <Link to={'/contact'}>Contacto</Link>
          </Menu.Item>
 
