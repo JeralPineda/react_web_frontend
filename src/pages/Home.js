ed46aeec265e8a950f62e6251542eaa165436045
Jeral <jeral.cris@gmail.com>
2021-08-30 22:31:20 -0600

    Agregando el componente de la seccion de reviews de usuarios a la pagina home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import MainBanner from 'components/Web/MainBanner';
 import HomeCourses from 'components/Web/HomeCourses';
 import HowMyCoursesWork from 'components/Web/HowMyCoursesWork';
+import ReviewsCourses from 'components/Web/ReviewsCourses';
 
 const Home = () => {
    return (
@@ -9,6 +10,7 @@ const Home = () => {
          <MainBanner />
          <HomeCourses />
          <HowMyCoursesWork />
+         <ReviewsCourses />
       </>
    );
 };
