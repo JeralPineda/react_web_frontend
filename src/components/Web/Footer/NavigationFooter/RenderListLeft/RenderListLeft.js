d8535ab6530a6b8553005311ed0d87a5acd3755d
Jeral <jeral.cris@gmail.com>
2021-08-31 12:29:27 -0600

    Lista de navegacion en la parte izquierda

---
+++ src/components/Web/Footer/NavigationFooter/RenderListLeft/RenderListLeft.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BookOutlined, CodeOutlined, DatabaseOutlined, RightOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+
+export const RenderListLeft = () => {
+   return (
+      <ul>
+         <li>
+            <a href='#'>
+               <BookOutlined /> Cursos online
+            </a>
+         </li>
+         <li>
+            <Link to='/contact'>
+               <CodeOutlined /> Desarrollo web
+            </Link>
+         </li>
+         <li>
+            <a href='#'>
+               <DatabaseOutlined /> Base de datos
+            </a>
+         </li>
+         <li>
+            <a href='#'>
+               <RightOutlined /> Política de privacidad
+            </a>
+         </li>
+      </ul>
+   );
+};
