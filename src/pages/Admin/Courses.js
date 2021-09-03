6ba9e65dd9130404b87d591f57540f1306d2fae7
Jeral <jeral.cris@gmail.com>
2021-09-03 17:35:42 -0600

    obteniendo todos los cursos y guardarlos en el estado

--- src/pages/Admin/Courses.js
+++ src/pages/Admin/Courses.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getCoursesApi } from 'api/courses';
 
 const Courses = () => {
+   const [courses, setCourses] = useState([]);
+   const [reloadCourses, setReloadCourses] = useState(false);
+
+   useEffect(() => {
+      getCoursesApi().then((response) => {
+         setCourses(response.courses);
+      });
+
+      setReloadCourses(false);
+   }, [reloadCourses]);
+
    return (
       <div className='courses'>
          <h1>Administrar cursos</h1>
