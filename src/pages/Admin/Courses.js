7460f2203f222676d4db07f5a7e19ee449562d5d
Jeral <jeral.cris@gmail.com>
2021-09-02 19:43:34 -0600

    Componente para la pagina donde se administraran los cursos

---
+++ src/pages/Admin/Courses.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Courses = () => {
+   return (
+      <div className='courses'>
+         <h1>Administrar cursos</h1>
+      </div>
+   );
+};
+
+export default Courses;
