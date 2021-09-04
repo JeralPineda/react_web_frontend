c00b02904b9266e67fe05e7c862a47033df21aee
Jeral <jeral.cris@gmail.com>
2021-09-03 22:07:55 -0600

    estructura del componente para mostrar la lista de los cursos

---
+++ src/components/Admin/Courses/CoursesList/CoursesList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CoursesList = ({ courses, setReloadCourses }) => {
+   console.log(courses);
+
+   return (
+      <div>
+         <h1>Courses List...</h1>
+      </div>
+   );
+};
+
+export default CoursesList;
