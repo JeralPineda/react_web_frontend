7215c3e2250160cbdb904839b2e7f73499ab8e1c
Jeral <jeral.cris@gmail.com>
2021-09-03 23:19:36 -0600

    Obteniendo los datos de los cursos de la API de Udemy

--- src/components/Admin/Courses/CoursesList/CoursesList.js
+++ src/components/Admin/Courses/CoursesList/CoursesList.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import { getCourseDataUdemyApi } from 'api/courses';
+
 const CoursesList = ({ courses, setReloadCourses }) => {
-   console.log(courses);
+   if (courses.length > 0) {
+      courses.forEach((course) => {
+         getCourseDataUdemyApi(course.idCourse).then((response) => {
+            console.log(response);
+         });
+      });
+   }
 
    return (
       <div>
