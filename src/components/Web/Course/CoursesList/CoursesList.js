f61aa0dabca32a4b7897273f96293defe5068a66
Jeral <jeral.cris@gmail.com>
2021-09-05 10:48:38 -0600

    Componente para la lista de cursos de la pagina cursos

---
+++ src/components/Web/Course/CoursesList/CoursesList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './CoursesList.scss';
+
+const CoursesList = () => {
+   return (
+      <div>
+         <h1>CoursesList</h1>
+      </div>
+   );
+};
+
+export default CoursesList;
