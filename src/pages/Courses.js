245c8ddbaa179ae671298ea1b6875ea3f28e3d7d
Jeral <jeral.cris@gmail.com>
2021-09-05 11:28:54 -0600

    cambio en la importacion del componente CoursesList

--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Row, Col, Spin, notification } from 'antd';
 
 import PresentationCourses from 'components/Web/Course/PresentationCourses';
-import CoursesList from 'components/Web/Course/PresentationCourses';
+import CoursesList from 'components/Web/Course/CoursesList';
 import { getCoursesApi } from 'api/courses';
 
 export const Courses = () => {
