8b3de37b82a15d8f68f233a052c415efe81d73de
Jeral <jeral.cris@gmail.com>
2021-09-05 13:20:33 -0600

    Pintando todos los cursos por pantalla

--- src/components/Web/Course/CoursesList/Course.js
+++ src/components/Web/Course/CoursesList/Course.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { notification } from 'antd';
+import { Button, Card, notification, Rate } from 'antd';
 
 import { getCourseDataUdemyApi } from 'api/courses';
 
 export const Course = ({ course }) => {
    const [courseInfo, setCourseInfo] = useState({});
+
+   const { Meta } = Card;
 
    useEffect(() => {
       getCourseDataUdemyApi(course.idCourse)
@@ -25,8 +27,27 @@ export const Course = ({ course }) => {
    }, [course]);
 
    return (
-      <div>
-         <h1>{courseInfo.title}</h1>
-      </div>
+      <a href='#' target='_blank' rel='noopener noreferrer'>
+         <Card
+            //
+            cover={<img src={courseInfo.image_480x270} alt={courseInfo.title} />}
+         >
+            <Meta
+               //
+               title={courseInfo.title}
+               description={courseInfo.headline}
+            />
+
+            <Button>Ir al curso</Button>
+
+            <div className='courses-list__course-footer'>
+               <span className='price'>{course.price ? `${course.price} US$` : courseInfo.price}</span>
+
+               <div>
+                  <Rate disabled defaultValue={5} />
+               </div>
+            </div>
+         </Card>
+      </a>
    );
 };
