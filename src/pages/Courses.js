ecf2365cd5d7bf1943c7d8ef372adc8a365dcd99
Jeral <jeral.cris@gmail.com>
2021-09-05 10:46:32 -0600

    Creando estructura de la pagina de cursos

--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Row, Col, Spin, notification } from 'antd';
+
+import PresentationCourses from 'components/Web/Course/PresentationCourses';
+import CoursesList from 'components/Web/Course/PresentationCourses';
+import { getCoursesApi } from 'api/courses';
 
 export const Courses = () => {
    return (
-      <div>
-         <h1>Courses</h1>
-      </div>
+      <Row>
+         <Col md={4} />
+         <Col md={16}>
+            <PresentationCourses />
+            <CoursesList />
+         </Col>
+         <Col md={4} />
+      </Row>
    );
 };
