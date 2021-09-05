97d14377222733f992c5571ebf450278ea0172db
Jeral <jeral.cris@gmail.com>
2021-09-04 18:47:27 -0600

    Pintando un listado con todos los cursos en el back office

--- src/components/Admin/Courses/CoursesList/CoursesList.js
+++ src/components/Admin/Courses/CoursesList/CoursesList.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { List, Button, Modal as ModalAntd, notification } from 'antd';
+import DragSortableList from 'react-drag-sortable';
 
 import { getCourseDataUdemyApi } from 'api/courses';
+import Modal from 'components/Modal';
+import { Course } from './Course';
+
+import './CoursesList.scss';
+
+const { confirm } = ModalAntd;
 
 const CoursesList = ({ courses, setReloadCourses }) => {
+   const [listCourses, setListCourses] = useState([]);
+   const [isVisibleModal, setIsVisibleModal] = useState(false);
+   const [modalTitle, setModalTitle] = useState('');
+   const [modalContent, setModalContent] = useState(null);
+
+   useEffect(() => {
+      const listCourseArray = [];
+      courses.forEach((course) => {
+         listCourseArray.push({
+            content: <Course course={course} />,
+         });
+      });
+      setListCourses(listCourseArray);
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+   }, [courses]);
+
+   const onSort = (sortedList, dropEvent) => {
+      console.log(sortedList);
+   };
+
    if (courses.length > 0) {
       courses.forEach((course) => {
          getCourseDataUdemyApi(course.idCourse).then((response) => {
@@ -12,8 +40,24 @@ const CoursesList = ({ courses, setReloadCourses }) => {
    }
 
    return (
-      <div>
-         <h1>Courses List...</h1>
+      <div className='courses-list'>
+         <div className='courses-list__header'>
+            <Button type='primary' onClick={() => console.log('Creando curso')}>
+               Nuevo curso
+            </Button>
+         </div>
+
+         <div className='courses-list__items'>
+            {listCourses.length === 0 && (
+               <h2
+                  //
+                  style={{ textAlign: 'center', margin: 0 }}
+               >
+                  No tienes cursos creados
+               </h2>
+            )}
+            <DragSortableList items={listCourses} onSort={onSort} type='vertical' />
+         </div>
       </div>
    );
 };
