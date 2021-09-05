c0e96f9a9cc731afc6c8f32beebed7dba3eeb49b
Jeral <jeral.cris@gmail.com>
2021-09-05 10:49:02 -0600

    Componente para la presentacion en la pagina de los cursos

---
+++ src/components/Web/Course/PresentationCourses/PresentationCourses.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './PresentationCourses.scss';
+
+const PresentationCourses = () => {
+   return (
+      <div>
+         <h1>PresentationCourses</h1>
+      </div>
+   );
+};
+
+export default PresentationCourses;
