175f4fc3781661861bcb3f5b75644f34bfd1e576
Jeral <jeral.cris@gmail.com>
2021-09-06 15:34:36 -0600

    Obteniendo los posts de la base de datos con paginacion

--- src/pages/Admin/Blog/Blog.js
+++ src/pages/Admin/Blog/Blog.js
@@ -1,15 +1,43 @@
 import React, { useEffect, useState } from 'react';
 import { Button, notification } from 'antd';
+import queryString from 'query-string';
+import { withRouter } from 'react-router';
 
 import Modal from 'components/Modal';
+import { getPostApi } from 'api/post';
 
 import './Blog.scss';
 
-const Blog = () => {
+const Blog = ({ location, history }) => {
+   const [posts, setPosts] = useState(null);
+   const [reloadPosts, setReloadPosts] = useState(false);
    const [isVisibleModal, setIsVisibleModal] = useState(false);
    const [modalTitle, setModalTitle] = useState('');
    const [modalContent, setModalContent] = useState(null);
 
+   const { page = 1 } = queryString.parse(location.search);
+
+   console.log(posts);
+
+   useEffect(() => {
+      getPostApi(12, page)
+         .then((response) => {
+            if (response?.code !== 200) {
+               notification['warning']({
+                  message: response.msg,
+               });
+            } else {
+               setPosts(response.posts);
+            }
+         })
+         .catch(() => {
+            notification['error']({
+               message: 'Error del servidor',
+            });
+         });
+
+      setReloadPosts(false);
+   }, [page, reloadPosts]);
    return (
       <div className='blog'>
          <div className='blog__add-post'>
@@ -30,4 +58,4 @@ const Blog = () => {
    );
 };
 
-export default Blog;
+export default withRouter(Blog);
