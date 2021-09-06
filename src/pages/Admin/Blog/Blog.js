67fa3bddf481c40835f325a1a64ad2a01244e1b9
Jeral <jeral.cris@gmail.com>
2021-09-06 14:11:32 -0600

    Creando la estructura basica de la seccio del blog

--- src/pages/Admin/Blog/Blog.js
+++ src/pages/Admin/Blog/Blog.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button, notification } from 'antd';
+
+import Modal from 'components/Modal';
 
 import './Blog.scss';
 
 const Blog = () => {
+   const [isVisibleModal, setIsVisibleModal] = useState(false);
+   const [modalTitle, setModalTitle] = useState('');
+   const [modalContent, setModalContent] = useState(null);
+
    return (
-      <div>
-         <h1>Blog</h1>
+      <div className='blog'>
+         <div className='blog__add-post'>
+            <Button type='primary'>Nuevo post</Button>
+         </div>
+
+         <h1>Post list..</h1>
+         <h2>Paginacion</h2>
+
+         <Modal
+            //
+            title={modalTitle}
+            isVisible={isVisibleModal}
+            setIsVisible={setIsVisibleModal}
+            width='75%'
+         />
       </div>
    );
 };
