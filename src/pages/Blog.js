8ca551aa02b3cc2d8a08125edcb9ae1623624044
Jeral <jeral.cris@gmail.com>
2021-09-07 15:38:40 -0600

    Estructura de la pagina para mostrar si es una lista de post o la informacion de un post

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -2,13 +2,27 @@ import React from 'react';
 import { Col, Row } from 'antd';
 import { useParams } from 'react-router-dom';
 
-export const Blog = ({}) => {
+import PostListWeb from 'components/Web/Blog/PostListWeb';
+import PostInfo from 'components/Web/Blog/PostInfo';
+
+export const Blog = ({ location, history }) => {
    const { url } = useParams();
 
    return (
-      <div>
-         {/* / */}
-         {url ? <h1>En un post</h1> : <h1>Lista de post</h1>}
-      </div>
+      <Row>
+         <Col md={4} />
+         <Col md={16}>
+            {url ? (
+               <PostInfo />
+            ) : (
+               <PostListWeb
+                  //
+                  location={location}
+                  history={history}
+               />
+            )}
+         </Col>
+         <Col md={4} />
+      </Row>
    );
 };
