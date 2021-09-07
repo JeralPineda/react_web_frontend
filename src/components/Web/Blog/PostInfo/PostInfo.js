5b8d09fda1e2bf099acbd11dd1030f2dd15ccff9
Jeral <jeral.cris@gmail.com>
2021-09-07 15:37:15 -0600

    Componente para mostrar la informacion de un post

---
+++ src/components/Web/Blog/PostInfo/PostInfo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './PostInfo.scss';
+
+const PostInfo = () => {
+   return (
+      <div>
+         <h1>PostInfo</h1>
+      </div>
+   );
+};
+
+export default PostInfo;
