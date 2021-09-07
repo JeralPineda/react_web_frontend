54098b0ce73cfd9a2a7c580ed50ed8dc684a60ac
Jeral <jeral.cris@gmail.com>
2021-09-07 15:03:28 -0600

    obtenemos la url y con una condicion definimos si es la lista de post o un post

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Col, Row } from 'antd';
+import { useParams } from 'react-router-dom';
 
-export const Blog = () => {
+export const Blog = ({}) => {
+   const { url } = useParams();
+
    return (
       <div>
-         <h1>Blog</h1>
+         {/* / */}
+         {url ? <h1>En un post</h1> : <h1>Lista de post</h1>}
       </div>
    );
 };
