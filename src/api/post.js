5c08a6d2ce61c4c27d4b0b6afe0e9448666f6997
Jeral <jeral.cris@gmail.com>
2021-09-06 20:49:54 -0600

    Funcion para consumir endpoint que crea un nuevo post

--- src/api/post.js
+++ src/api/post.js
@@ -37,3 +37,27 @@ export const deletePostApi = (token, id) => {
          return err;
       });
 };
+
+export const addPostApi = (token, post) => {
+   const url = `${basePath}/${apiVersion}/post/add-post`;
+
+   const params = {
+      method: 'POST',
+      headers: {
+         'Content-Type': 'application/json',
+         'x-token': `Bearer ${token}`,
+      },
+      body: JSON.stringify(post),
+   };
+
+   return fetch(url, params)
+      .then((response) => {
+         return response.json();
+      })
+      .then((result) => {
+         return result;
+      })
+      .catch((err) => {
+         return err;
+      });
+};
